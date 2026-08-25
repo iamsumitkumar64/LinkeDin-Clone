167b821bee366aa04cdb8b7d473e24b79651a60a
Sumit Kumar <iam.sumitkumar64@gmail.com>
2026-08-25 16:04:45 +0530

    fix

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -3,10 +3,11 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
+  async getHello() {
+    await fetch('https://birwal-kart.onrender.com');
     return this.appService.getHello();
   }
 }
